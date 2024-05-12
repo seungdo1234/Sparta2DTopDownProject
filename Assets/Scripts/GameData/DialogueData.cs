46296fcca4d6af83c00d2d410a4fe83d1f09527f
seungdo1234 <wltmdeh12@naver.com>
2024-05-12 16:19:15 +0900

    주석 추가

--- Assets/Scripts/GameData/DialogueData.cs
+++ Assets/Scripts/GameData/DialogueData.cs
@@ -1,13 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
-
 [System.Serializable]
 public class DialogueData 
 {
-    private int dialogueStep = 0;
-    public string[] dialogues;
+    private int dialogueStep = 0; // 대화 단계 
+    public string[] dialogues; // 단계별 대화를 저장하는 문자열 배열
 
-    public string GetDialogue()
+    public string GetDialogue() // 대화 반환
     {
         if(dialogues.Length <= dialogueStep)
         {
@@ -17,12 +14,12 @@ public class DialogueData
         return dialogues[dialogueStep++];
     }
 
-    public void ResetDialogue()
+    public void ResetDialogue() // 댜화 단계 초기화
     {
         dialogueStep = 0;
     }
 
-    public bool DialogueComplete()
+    public bool DialogueComplete() // 대화가 끝났는 지 반환
     {
         return dialogues.Length <= dialogueStep;
     }
--- Assets/Scripts/MainScene/GamePlay/Behaviors/PlayerDetectionTrigger.cs
+++ Assets/Scripts/MainScene/GamePlay/Behaviors/PlayerDetectionTrigger.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 // NPC 상호작용 관련 플레이어 탐지 클래스
 public class PlayerDetectionTrigger : MonoBehaviour
 {
-    private TopDownController controler;
+    private TopDownController controller;
     [SerializeField] private NpcDialogueHandler npcDialogueHandler;
     [SerializeField] private RectTransform interactionIcon; // 상호 작용 가능 UI
     [SerializeField] private Vector3 interactionIconPosition; // 상호 작용 가능 UI가 표시될 위치 
@@ -13,7 +13,7 @@ public class PlayerDetectionTrigger : MonoBehaviour
     private bool isPlayerInside = false; // 플레이어가 범위 안으로 들어왔다면 true
     private void Start()
     {
-        controler = EntityDataManager.Instance.PlayerData.GetComponent<TopDownController>();
+        controller = EntityDataManager.Instance.PlayerData.GetComponent<TopDownController>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision) // NPC 상호작용 활성화
@@ -27,7 +27,7 @@ public class PlayerDetectionTrigger : MonoBehaviour
         StartCoroutine(InteractionIconPositionUpdater());
         interactionIcon.gameObject.SetActive(true);
 
-        controler.OnInteractEvent += npcDialogueHandler.DialogueEvent;
+        controller.OnInteractEvent += npcDialogueHandler.DialogueEvent;
     }
     private void OnTriggerExit2D(Collider2D collision) // NPC 상호작용 비활성화
     {
@@ -38,7 +38,7 @@ public class PlayerDetectionTrigger : MonoBehaviour
 
         isPlayerInside = false;
         interactionIcon.gameObject.SetActive(false);
-        controler.OnInteractEvent -= npcDialogueHandler.DialogueEvent;
+        controller.OnInteractEvent -= npcDialogueHandler.DialogueEvent;
     }
 
     private IEnumerator InteractionIconPositionUpdater() // 상호작용 UI 위치를 NPC 옆에 표시
