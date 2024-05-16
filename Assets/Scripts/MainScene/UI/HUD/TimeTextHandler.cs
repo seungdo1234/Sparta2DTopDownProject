a17d02866c39cb59c49c944a3667c79e2bc90fc4
seungdo1234 <86179438+seungdo1234@users.noreply.github.com>
2024-05-16 10:46:31 +0900

    에셋 수정

--- Assets/Scripts/MainScene/UI/HUD/TimeTextHandler.cs
+++ Assets/Scripts/MainScene/UI/HUD/TimeTextHandler.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Unity.VisualScripting;
 
 public class TimeTextHandler : MonoBehaviour
 {
@@ -29,4 +30,7 @@ public class TimeTextHandler : MonoBehaviour
             yield return new WaitForSeconds(waitTime);
         }
     }
+    
+    
+    
 }
